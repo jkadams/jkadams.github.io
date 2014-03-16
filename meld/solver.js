aa909c22b9083a1576a36aaebe64144db2ff4173
Jon Adams <jkadams@users.noreply.github.com>
2014-03-16 17:08:43 -0400

    Try bonus cards on the first turn

--- meld/solver.js
+++ meld/solver.js
@@ -9,12 +9,12 @@ var times = [];
 var evalcount = [];
 
 Solver.prototype.findBestMove = function(game) {
-  //var startTime = new Date();
+  var startTime = new Date();
   evaluated = 0;
   this.moveScores(game, 0);
-  //var endTime = new Date();
-  //times.push(endTime-startTime);
-  //evalcount.push(evaluated);
+  var endTime = new Date();
+  times.push(endTime-startTime);
+  evalcount.push(evaluated);
   switch (this.nextMove) {
     case 0:
       return 'LEFT';
@@ -55,6 +55,15 @@ Solver.prototype.moveScores = function(game, depthGone) {
   if (depthGone == this.depthNewCards + this.depthNoNewCards) {
     return Solver.finalBoardScore(game);
   }
+  
+  // If we're using the nextValue, try 1/2/3.
+  // Otherwise, just use 1 and it will go unused.
+  var nextPieceRange = depthGone < this.depthNewCards - 1 ? 3 : 1;
+  // If the next card is a bonus card, and we're at depth 0, try all possible.
+  var bonusMax = 6;
+  if (depthGone == 0 && game.nextValue == MeldGame.NEXT_BONUS) {
+    bonusMax = tempGame.highestValue() / 8;
+  }
   var bestMove = null;
   var bestScore = Solver.LOSE_SCORE;
   for (var moveDirection = 0; moveDirection < 4; moveDirection++) {
@@ -63,23 +72,25 @@ Solver.prototype.moveScores = function(game, depthGone) {
     var sum = 0;
     var count = 0;
     var expectedScore;
-    // If we're using the nextValue, try 1/2/3.
-    // Otherwise, just use 1 and it will go unused.
-    var maxNextPiece = depthGone < this.depthNewCards - 1 ? 3 : 1;
     if (newLocations != 0) {
       if (depthGone < this.depthNewCards) {
+        // Add new cards at this depth.
         for (var newLocation = 0; newLocation < 4; newLocation++) {
-          if (newLocations & (1 << newLocation)) {
-            for (var nextPiece = 1; nextPiece <= maxNextPiece; nextPiece++) {
+          if ((newLocations & (1 << newLocation)) == 0) {
+            continue; 
+          }
+          for (var nextPiece = 1; nextPiece <= nextPieceRange; nextPiece++) {
+            for (var bonus = 6; bonus <= bonusMax; bonus *= 2) {
               var randomGame = tempGame.copy();
-              randomGame.respondToUser(moveDirection, newLocation, nextPiece, 6);
+              randomGame.respondToUser(moveDirection, newLocation, nextPiece, bonus);
               sum += this.moveScores(randomGame, depthGone + 1);
               count++;
             }
           }
         }
-        expectedScore = sum / count;
+        expectedScore = sum / count; // should we use the undrawn stack instead?
       } else {
+        // Ignore new cards, just try to get rid of existing cards.
         expectedScore = this.moveScores(tempGame, depthGone + 1);
       }
 
